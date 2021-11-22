aee1759d79147b8a9569a3dd66766823bf0d5835
dva5606 <86691169+dva5606@users.noreply.github.com>
2021-11-23 11:34:58 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "PEACH") {
-        basic.showString("PEACH")
-        if (PEACH == true) {
-            PEACH = false
-        } else if (PEACH == false) {
-            PEACH = true
+        if (true) {
+            servos.P0.setAngle(180)
+        }
+        if (Grape__Strawberry == true) {
+            Grape__Strawberry = false
+        } else if (Grape__Strawberry == false) {
+            Grape__Strawberry = true
         }
     }
     if (receivedString == "MANGO") {
-        basic.showString("MANGO")
-        if (MANGO == true) {
-            MANGO = false
-        } else if (MANGO == false) {
-            MANGO = true
+        if (true) {
+            pins.servoWritePin(AnalogPin.P14, 180)
+        }
+        if (Peach__Wild_Berry == true) {
+            Peach__Wild_Berry = false
+        } else if (Peach__Wild_Berry == false) {
+            Peach__Wild_Berry = true
         }
     }
 })
-let MANGO = false
-let PEACH = false
-basic.showIcon(IconNames.Yes)
-radio.setGroup(69)
+let Peach__Wild_Berry = false
+let Grape__Strawberry = false
 servos.P0.setAngle(90)
+pins.servoWritePin(AnalogPin.P14, 90)
