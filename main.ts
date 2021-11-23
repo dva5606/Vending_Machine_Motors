37bc2f4efc6af5a8dc6454e3f5023322d93e9369
dva5606 <86691169+dva5606@users.noreply.github.com>
2021-11-24 08:42:21 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ radio.onReceivedString(function (receivedString) {
         if (true) {
             servos.P0.setAngle(180)
         }
-        if (Grape__Strawberry == true) {
-            Grape__Strawberry = false
-        } else if (Grape__Strawberry == false) {
-            Grape__Strawberry = true
+        if (Cherry__Watermelon == true) {
+            Cherry__Watermelon = false
+        } else if (Cherry__Watermelon == false) {
+            Cherry__Watermelon = true
         }
     }
     if (receivedString == "MANGO") {
@@ -21,6 +21,6 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 let Peach__Wild_Berry = false
-let Grape__Strawberry = false
+let Cherry__Watermelon = false
 servos.P0.setAngle(90)
 pins.servoWritePin(AnalogPin.P14, 90)
